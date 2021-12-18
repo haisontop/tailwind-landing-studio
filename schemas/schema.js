c5c1cbd4f1411914a1684f440da4d55ab2fafd4f
haisontop <haisontop@gmail.com>
2021-12-18 06:56:10 -0800

    services page

--- schemas/schema.js
+++ schemas/schema.js
@@ -10,6 +10,7 @@ import blockContent from "./blockContent";
 import blog from "./blog";
 import team from "./team";
 import hero from "./hero";
+import services from "./services";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -26,6 +27,7 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
-    hero
+    hero,
+    services
   ]),
 });
---
+++ schemas/services.js
@@ -0,0 +1,85 @@
+export default {
+  name: "services",
+  title: "Services",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "heroText",
+      title: "Hero Text",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "offering",
+      title: "Our Offering",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          name: "offering",
+          title: "Offering",
+          fields: [
+            {
+              name: 'name',
+              type: 'string',
+              title: 'Name',
+            },
+            {
+              name: 'description',
+              type: 'string',
+              title: 'Description',
+            },
+            {
+              name: 'mobileIcon',
+              type: 'image',
+              title: 'Mobile Icon',
+              options: {
+                hotspot: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "specialities",
+      title: "Our specialities",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          name: "specialist",
+          title: "Specialist",
+          fields: [
+            {
+              name: 'name',
+              type: 'string',
+              title: 'Name',
+            },
+            {
+              name: 'backgroundImage',
+              type: 'image',
+              title: 'Background image',
+              options: {
+                hotspot: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
